594adc22169133dcdea2d9ffe50a560a60916856
James <james1239090@gmail.com>
2017-04-26 14:08:28 +0800

    create a seed for the test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+create_products = for i in 1..50 do
+    Product.create!([name: "Product no.#{i}"])
+end
